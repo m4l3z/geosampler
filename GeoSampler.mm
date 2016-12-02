e184a144368b6c77954419657469aa8826e5c287
tatoufff <pierre.etheve.lfdv@gmail.com>
2016-12-02 16:54:26 +0100

    Ajout de la mindmap de brainstorm

---
+++ GeoSampler.mm
@@ -0,0 +1,97 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="GeoSampler" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1480693676584"><hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" STYLE_REF="#Vert" LAST="false">
+            <icon_contained_condition ICON="button_ok"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="#Jaune" LAST="false">
+            <icon_contained_condition ICON="bookmark"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="#Rouge" LAST="false">
+            <icon_contained_condition ICON="messagebox_warning"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="400" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="#Titre" COLOR="#ad0000" STYLE="fork">
+<font NAME="Liberation Serif" SIZE="13" BOLD="true"/>
+<edge STYLE="bezier" COLOR="#808080"/>
+</stylenode>
+<stylenode TEXT="#Vert" BACKGROUND_COLOR="#d7feb5"/>
+<stylenode TEXT="#Rouge" BACKGROUND_COLOR="#febab5"/>
+<stylenode TEXT="#Jaune" BACKGROUND_COLOR="#fef5b5"/>
+<stylenode TEXT="Code">
+<font NAME="CMU Typewriter Text Variable Width" SIZE="11" BOLD="true"/>
+</stylenode>
+<stylenode TEXT="#Violet" BACKGROUND_COLOR="#eac7f5"/>
+<stylenode TEXT="#Gris" BACKGROUND_COLOR="#dfdfdf"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Interface command line" POSITION="right" ID="ID_572422127" CREATED="1480693743025" MODIFIED="1480693765232">
+<node TEXT="1e impl&#xe9;mentation" ID="ID_1339818215" CREATED="1480693878441" MODIFIED="1480693885572">
+<node TEXT="Add samples" ID="ID_1237019465" CREATED="1480693770760" MODIFIED="1480693775290"/>
+<node TEXT="Create polygon" ID="ID_1567738054" CREATED="1480693777303" MODIFIED="1480693781691">
+<node TEXT="n nodes" ID="ID_1926004505" CREATED="1480693781979" MODIFIED="1480693785457"/>
+</node>
+<node TEXT="add_node" ID="ID_1121731938" CREATED="1480693793959" MODIFIED="1480693796092"/>
+<node TEXT="remove node" ID="ID_339002632" CREATED="1480693796453" MODIFIED="1480693799058"/>
+<node TEXT="play" ID="ID_195163520" CREATED="1480693807100" MODIFIED="1480693812336"/>
+<node TEXT="stop" ID="ID_1985872924" CREATED="1480693812561" MODIFIED="1480693813918"/>
+<node TEXT="set node sample" ID="ID_852403966" CREATED="1480693799761" MODIFIED="1480693802887"/>
+</node>
+<node TEXT="2e vague" ID="ID_1683826965" CREATED="1480693937472" MODIFIED="1480693941904">
+<node TEXT="Load/Save project" ID="ID_1747418131" CREATED="1480693766286" MODIFIED="1480693770544"/>
+<node TEXT="pause" ID="ID_796940031" CREATED="1480693814120" MODIFIED="1480693814963"/>
+<node TEXT="mute polygon" ID="ID_1941257618" CREATED="1480693829152" MODIFIED="1480693844119"/>
+<node TEXT="copy/paste node" ID="ID_1971502487" CREATED="1480693862954" MODIFIED="1480693866384"/>
+<node TEXT="export/import poly to base" ID="ID_1989896178" CREATED="1480693898766" MODIFIED="1480693909963"/>
+</node>
+</node>
+</node>
+</map>
