47fffceba10fa32107a1ae22844f4867ecd369bc
m4l3z <best_ness@hotmail.fr>
2016-12-09 17:53:34 +0100

    Load & save polygon (unfinished)

--- polygon.py
+++ polygon.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from node import Node
+from lxml import etree
 
 class Polygon: 
     name = None
@@ -11,14 +12,16 @@ class Polygon:
     def __init__(self, name, dimension):
         self.name = name 
         self.dimension = dimension
-        reset()
+        self.reset()
 
     def show_details(self):
-        print("Name:",self.name,"\nDimension:",len(self.nodes))
-        print("\nNodes:")
-        for n in self.nodes:
-            n.show_details()
-
+        print("<Polygon dim =",'"', str(self.dimension),'"', " name =" ,self.name,">\n")
+        for node in self.nodes:
+            if(node.sample == None):
+                print("<node></node>\n")
+            else:
+                print("<node> path ="+node.sample+"</node>\n")
+        print("</Polygon>")
     def append_node(self):
         self.nodes.append(Node(len(self.nodes)))
 
@@ -29,4 +32,28 @@ class Polygon:
         return self.nodes[pos]
 
     def reset(self):
-        self.nodes = [None for i in range(self.dimension)]
+        self.nodes = [Node() for i in range(self.dimension)]
+    
+    def save(self): 
+        f = open(self.name+".save","w+")
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write("<Polygon dim ="+ '"'+str(self.dimension)+'"'+ " name =" +self.name+">\n")
+        for node in self.nodes:
+            if(node.sample == None):
+                f.write("<node></node>\n")
+            else:
+                f.write("<node> path ="+node.sample+"</node>\n")
+        f.write("</Polygon>")
+        f.close()
+    def load(self,filename):
+        tree = etree.parse(filename)
+        for Polygon in tree.xpath("/Polygon"):
+            print(Polygon.name)
+        
+
+
+polya = Polygon("polya",5)
+polya.nodes[1].sample="Samples/Xylophone.wav"
+polya.show_details()
+polya.save()
+polya.load("polya.save")
